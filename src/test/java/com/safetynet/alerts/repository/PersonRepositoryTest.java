6836c8768ac43adc6103f847d1c68f31fa05790d
ludo <ludovic.brot@gmail.com>
2026-02-09 18:07:47 +0100

    Ajout de test sur les Repositories pour meilleur couverture

--- src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
+++ src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
@@ -2,6 +2,7 @@ package com.safetynet.alerts.repository;
 
 import com.safetynet.alerts.model.Person;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -26,31 +27,36 @@ public class PersonRepositoryTest {
                 new Person("Tenley", "Boyd", "1509 Culver St", "Culver", "97451", "841-874-6512", "tenley.boyd@email.com")
         )));
     }
-
+    @Test
      void getAll_ShouldReturnAllPersons() {
         List<Person> persons = personRepository.getAll();
         assert persons.size() == 3;
      }
+     @Test
      void geAllByAddress_ShouldReturnPersons() {
         List<Person> persons = personRepository.getAllByAddress("1509 Culver St");
         assert persons.size() == 3;
      }
-
+     @Test
       void findByFirstNameAndLastName_ShouldReturnPerson() {
         Person person = personRepository.findByFirstnameAndLastname("John", "Boyd").orElse(null);
         assert person != null;
         assert person.getFirstName().equals("John");
         assert person.getLastName().equals("Boyd");
      }
-
+     @Test
       void findByFirstNameAndLastName_ShouldReturnEmpty() {
         assert personRepository.findByFirstnameAndLastname("Jane", "Doe").isEmpty();
      }
+
+     @Test
       void save_ShouldAddPerson() {
           Person newPerson = new Person("Jane", "Doe", "123 Main St", "Anytown", "12345", "555-555-5555", "");
           personRepository.save(newPerson);
           assert personRepository.getAll().size() == 4;
       }
+
+      @Test
         void delete_ShouldRemovePerson() {
             Person person = personRepository.findByFirstnameAndLastname("John", "Boyd").orElse(null);
             assert person != null;
